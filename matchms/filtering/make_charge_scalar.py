9db970723fc1983d3872b676cd1ff9b98652a890
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-15 20:05:56 +0200

    merge from PR #207

--- matchms/filtering/make_charge_scalar.py
+++ matchms/filtering/make_charge_scalar.py
@@ -12,4 +12,13 @@ def make_charge_scalar(spectrum_in: SpectrumType) -> SpectrumType:
     if isinstance(spectrum.get("charge", None), list):
         spectrum.set("charge", int(spectrum.get("charge")[0]))
 
+    # Interpret charge when parsed as string
+    if isinstance(spectrum.get("charge", None), str):
+        charge = spectrum.get("charge").strip("+ ")
+        if '-' in charge:
+            charge = -int(charge.strip("-+"))
+        else:
+            charge = int(charge)
+        spectrum.set("charge", charge)
+
     return spectrum
