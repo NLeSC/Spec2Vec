c41d116d909dbce073ecb6093544788d5ea604ea
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-17 16:53:57 +0200

    extend cleaning options: better split soon!

--- matchms/filtering/clean_compound_name.py
+++ matchms/filtering/clean_compound_name.py
@@ -9,12 +9,27 @@ def clean_compound_name(spectrum_in: SpectrumType) -> SpectrumType:
     This function will look for an adduct string and remove it.
     A list of frequently seen name additions that do not belong to the compound
     name will be removed."""
+    def remove_formula(name):
+        "Find formula string at end of name and remove it."
+        if re.search(r"^[C][0-9][0-9A-Z]{4,}$", name.split(" ")[-1]):
+            formula = name.split(" ")[-1]
+            # Remove formula from name
+            name = " ".join(name.split(" ")[:-1])
+            # Add formula to metadata if not present yet
+            if spectrum.get("formula", None) is None:
+                spectrum.set("formula", formula)
+                print("Added formula ({}) to metadata.".format(formula))
+        return name
+
     def remove_adduct(name):
         """Find and remove adduct string."""
         potential_adduct = name.split(' ')[-1].strip().replace('*', '')
         if looks_like_adduct(potential_adduct):
-            assert spectrum.get("adduct", None) is not None, ("Adduct found in compound name but not in metadata.",
-                                                              "Apply 'add_adduct' filter first.")
+            # assert spectrum.get("adduct", None) is not None, ("Adduct found in compound name but not in metadata.",
+            #                                                   "Apply 'add_adduct' filter first.")
+            if spectrum.get("adduct", None) is None:
+                spectrum.set("adduct", potential_adduct)
+                print("Added adduct to metadata:", potential_adduct)
             name_split = name.split(" ")
             name = " ".join(name_split[:-1])
         return name
@@ -59,8 +74,10 @@ def clean_compound_name(spectrum_in: SpectrumType) -> SpectrumType:
 
     if name:
         # Clean found name string
-        name_cleaned = remove_adduct(name)
+        name_cleaned = remove_formula(name)
+        name_cleaned = remove_adduct(name_cleaned)
         name_cleaned = remove_non_compound_name_parts(name_cleaned)
+        name_cleaned = name_cleaned.strip("; ")
         if name_cleaned != name:
             spectrum.set("compound_name", name_cleaned)
             print("Added cleaned compound name:", name_cleaned)
